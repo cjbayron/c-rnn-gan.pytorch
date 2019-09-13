2529c743bb2ead06863a099e14d13f68afee78cc
CJ Bayron <cjdbayron@gmail.com>
2019-09-13 16:36:33 +0800

    add generator, change optimizers to adam

---
+++ generate.py
@@ -0,0 +1,44 @@
+''' Generate MIDI from trained C-RNN-GAN generator
+'''
+import os
+import torch
+
+from c_rnn_gan import Generator
+import music_data_utils
+
+CKPT_DIR = 'models'
+G_FN = 'c_rnn_gan_g.pth'
+MAX_SEQ_LEN = 200
+FILENAME = 'sample.mid'
+
+def generate():
+    '''
+    '''
+    # prepare model
+    dataloader = music_data_utils.MusicDataLoader(datadir=None)
+    num_feats = dataloader.get_num_song_features()
+
+    use_gpu = torch.cuda.is_available()
+    g_model = Generator(num_feats, use_cuda=use_gpu)
+
+    ckpt = torch.load(os.path.join(CKPT_DIR, G_FN))
+    g_model.load_state_dict(ckpt)
+
+    # generate from model then save to MIDI file
+    g_states = g_model.init_hidden(1)
+    z = torch.empty([1, MAX_SEQ_LEN, num_feats]).uniform_() # random vector
+    if use_gpu:
+        z = z.cuda()
+        g_model.cuda()
+
+    g_model.eval()
+    g_feats, _ = g_model(z, g_states)
+    song_data = g_feats.squeeze().cpu()
+    song_data = song_data.detach().numpy()
+    
+    dataloader.save_data(FILENAME, song_data)
+    print('Generated {}'.format(FILENAME))
+
+
+if __name__ == "__main__":
+    generate()
